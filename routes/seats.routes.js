70b57f2d0e51b89b4b4775261605f6a0de697b07
Natalia Banasiewicz <natalia.banasiewicz@wp.pl>
2020-04-23 09:50:19 +0200

    Add api/seats endpoints

---
+++ routes/seats.routes.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const db = require("./../db");
+const uuid = require('uuid');
+
+// prepare empty router
+const router = express.Router();
+
+// get all seats from db
+router.route("/seats").get((req, res, next) => {
+  res.json(db.seats);
+});
+
+// get seat with chosen id or random
+router.route("/seats/:id").get((req, res, next) => {
+  const id = req.params.id;
+  let result;
+  if (id === "random") {
+    let index = Math.floor(Math.random() * db.seats.length);
+    console.log(index);
+    result = db.seats[index];
+  } else {
+    result = db.seats.filter((el) => {
+      return el.id == id;
+    });
+  }
+  res.json(result);
+});
+
+// post new seat with uuid generated id
+
+// { id: 6, day: 2, seat: 2, client: 'Molier Lo Celso', email: 'moiler.lo.celso@example.com' },
+router.route("/seats").post((req, res, next) => {
+  randomId = uuid.v4();
+  db.seats.push({
+    id: randomId,
+    day: req.body.day,
+    seat: req.body.seat,
+    client: req.body.client,
+    email: req.body.email,
+  });
+  res.json({ message: "OK" });
+});
+
+// edit existing seat using its id
+router.route("/seats/:id").put((req, res, next) => {
+  const id = req.params.id;
+  db.seats.map((el) => {
+    if (el.id == id) {
+    el.day = req.body.day;
+    el.seat = req.body.seat;
+    el.client = req.body.client;
+    el.email = req.body.email;
+    }
+    return el;
+  });
+  res.json({ message: "OK" });
+});
+
+// delete existing seat using its id
+router.route("/seats/:id").delete((req, res, next) => {
+  const id = req.params.id;
+  const seat = db.seats.filter((el) => el.id == id);
+  const index = db.seats.indexOf(seat[0]);
+  db.seats.splice(index, 1);
+  res.json({ message: "OK" });
+});
+
+module.exports = router;
